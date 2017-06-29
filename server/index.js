5bd6810e928224069d9aa543951515066d656756
Eric W. Greene <eric@ericwgreene.com>
2017-06-29 08:03:54 -0700

    fixed package json scripts

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
+import fs from 'fs';
+import path from 'path';
 import http from 'http';
 import express from 'express';
 import graphqlHttp from 'express-graphql';
+import { printSchema } from 'graphql';
 
 import { schema } from './schema';
+
+const schemaPath = path.resolve(__dirname, '../schema.graphql');
+fs.writeFileSync(schemaPath, printSchema(schema));
+console.log('Wrote Schema: ' + schemaPath);
 
 const port = 3020;
 
